42c45ac8a60af5d065ec08db3c2dd897421e93e8
Valeriy Shubin <b-boy56@mail.ru>
2024-04-20 16:01:43 +0300

    Initial commit

---
+++ src/main/java/ru/netology/hibernate/controller/MyController.java
@@ -0,0 +1,33 @@
+package ru.netology.hibernate.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.netology.hibernate.entity.Person;
+import ru.netology.hibernate.model.PersonForRequest;
+import ru.netology.hibernate.service.MyService;
+
+@RestController
+public class MyController {
+    private final MyService service;
+    private String name;
+    private String surname;
+    private Integer age;
+    private String phoneNumber;
+    private String cityOfLiving;
+
+    public MyController(MyService service) {
+        this.service = service;
+    }
+
+    @PostMapping("add/person")
+    public Person addPerson(@RequestBody PersonForRequest personForRequest) {
+        System.out.println(personForRequest);
+        return service.addPerson(personForRequest.getPerson());
+    }
+
+    @GetMapping("persons/by-city")
+    public ResponseEntity<String> getPersonsByCity(@RequestParam String city) {
+        return service.getPersonsByCity(city);
+    }
+
+}
